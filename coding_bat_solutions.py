851f525560bd21b116dd3c7fc0d5d9c47465bea3
Akilan Chandrasekaran <akilanmg@gmail.com>
2019-12-03 16:25:23 +0530

    Included xyz search problem

--- coding_bat_solutions.py
+++ coding_bat_solutions.py
@@ -1,3 +1,5 @@
+# Each Problem has many solutions, presenting just one of them here. 
+
 # Return the number of times that the string "code" appears anywhere in the given string variable (text),
 # except we'll accept any letter for the 'd', so "cope" and "cooe" count.
 
@@ -10,3 +12,19 @@ for x in text:
         count = count +1
     i += 1
 print(count)
+
+# Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period
+# So "xxyz" counts but "x.xyz" does not.
+
+xyz_string = 'abc.xyzxyz'
+dot_list = xyz_string.split('.')  # Split all the texts by period wise
+xyz_check = False
+i = 0
+for x in dot_list:
+    if i == 0:  # This section is meant for the first block of string, as it is not prefixed by a .dot Eg.'xyz.abc'
+        i += 1
+        if 'xyz' in x:
+            xyz_check = True
+    elif 'xyz' in x[1:]:
+        xyz_check = True
+print(xyz_check)
