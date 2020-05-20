ef37f4f29d52ba8be5824feaf52eedaea6575ed5
Akilan Chandrasekaran <akilanmg@gmail.com>
2020-05-20 06:52:54 -0400

    Update zero2hero.py

--- zero2hero.py
+++ zero2hero.py
@@ -1,3 +1,12 @@
+# LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, 
+# but returns the greater if one or both numbers are odd
+def lesser_of_two_evens(a, b):
+  if a % 2 == 0 and b % 2 == 0:
+    return min(a, b)
+  return max(a, b)
+
+print(lesser_of_two_evens(2, 10))
+
 # ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
 def animal_crackers(text):
   animal_list = list(text.split())
